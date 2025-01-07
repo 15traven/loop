f50a4eb9311cc50a7c293e75c023daf9e09a1a64
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-01-07 22:43:44 +0200

    feat: create icon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use tray_icon::TrayIconBuilder;
+
+fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/icon.png");
+    let icon = load_icon(std::path::Path::new(path));
+
+    let tray_icon = TrayIconBuilder::new()
+        .with_tooltip("tray")
+        .with_icon(icon)
+        .build()
+        .unwrap();
+
+    loop {}
 }
