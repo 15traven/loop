5499fe16bba449fc6f2040308c7be575dcc56921
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-01-10 17:48:24 +0200

    Revert "feat: edit tray-icon crate; add event loop; add tray menu"
    This reverts commit 472fedb5d348d3763adde18482e11aba49d0c362.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,13 @@
-use std::{thread::{self, sleep}, time::Duration};
-use tao::{
-    event::Event,
-    event_loop::{ControlFlow, EventLoopBuilder},
-};
-use tray_icon::{
-    menu::{
-        AboutMetadata, Menu, MenuEvent, 
-        MenuItem, PredefinedMenuItem
-    }, 
-    TrayIcon, TrayIconBuilder, Icon
-};
+use std::time::Duration;
+use tokio::time::sleep;
+use tray_icon::TrayIconBuilder;
 
-enum UserEvent {
-    MenuEvent(tray_icon::menu::MenuEvent),
-}
-
-fn check_connection(
-    mut tray_icon: TrayIcon,
-    connected_icon: Icon,
-    disconnected_icon: Icon
-) {    
-    thread::spawn(move || {
-        loop {
-            let res = reqwest::blocking::get("https://google.com");
-            match res {
-                Ok(_)=> {
-                    tray_icon.set_icon(Some(connected_icon.clone())).unwrap();
-                }
-                Err(_err) => {
-                    tray_icon.set_icon(Some(disconnected_icon.clone())).unwrap();
-                }
-            }
-            sleep(Duration::from_secs(20));
-        }
-    });
+async fn check_connection() -> Result<(), reqwest::Error>{
+    let res = reqwest::get("https://google.com").await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err)
+    }
 }
 
 fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
@@ -56,55 +30,20 @@ async fn main() {
     let disconnected_icon_path = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/disconnected_icon.png");
     let disconnected_icon = load_icon(std::path::Path::new(disconnected_icon_path));
 
-    let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
+    let tray_icon = TrayIconBuilder::new()
+        .with_icon(connected_icon.clone())
+        .build()
+        .unwrap();
 
-    let proxy = event_loop.create_proxy();
-    MenuEvent::set_event_handler(Some(move |evnet| {
-        let _ = proxy.send_event(UserEvent::MenuEvent(evnet));
-    }));
-
-    let tray_menu = Menu::new();
-    let exit_item = MenuItem::new("Exit", true, None);
-    let _ = tray_menu.append_items(&[
-        &PredefinedMenuItem::about(
-            None, 
-            Some(AboutMetadata {
-                name: Some("Mercury".to_string()),
-                ..Default::default()
-            })
-        ),
-        &PredefinedMenuItem::separator(),
-        &exit_item
-    ]);
-
-    let mut tray_icon: Option<TrayIcon> = None;
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::NewEvents(tao::event::StartCause::Init) => {
-                tray_icon = Some(
-                    TrayIconBuilder::new()
-                        .with_menu(Box::new(tray_menu.clone()))
-                        .with_icon(connected_icon.clone())
-                        .build()
-                        .unwrap()
-                );
-
-                check_connection(
-                    tray_icon.as_ref().unwrap().clone(), 
-                    connected_icon.clone(), 
-                    disconnected_icon.clone()
-                );
+   loop {
+        match check_connection().await {
+            Ok(_) => {
+                let _ = tray_icon.set_icon(Some(connected_icon.clone()));
             }
-            Event::UserEvent(UserEvent::MenuEvent(event)) => {
-                if event.id == exit_item.id() {
-                    tray_icon.take();
-                    *control_flow = ControlFlow::Exit;
-                }
+            Err(_err) => {
+                let _ = tray_icon.set_icon(Some(disconnected_icon.clone()));
             }
-            _ => {}
         }
-    })
+        sleep(Duration::from_secs(20)).await;
+   }
 }
