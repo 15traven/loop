c6bf1b0780381550487b9d7a01184d5d21c0eaad
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-02-01 16:12:49 +0200

    feat: configure window

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,10 +1,15 @@
+use iced::Size;
+
 mod context;
 use context::Context;
 
 pub fn run() -> iced::Result {
-    iced::run(
+    iced::application(
         "Loop - history", 
         Context::update,
         Context::view
     )
+    .resizable(false)
+    .window_size(Size::new(350.0, 450.0))
+    .run()
 }
