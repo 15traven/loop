00f9bc5f8a8852e6c0ec5376257ef8064657541a
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-02-06 15:54:44 +0200

    fix: use local time instead of Utc

--- history/src/types/record.rs
+++ history/src/types/record.rs
@@ -1,24 +1,24 @@
 use serde::{Serialize, Deserialize};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, offset};
 use super::ConnectionStatus;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct HistoryRecord {
-    pub timestamp: DateTime<Utc>,
+    pub timestamp: DateTime<offset::Local>,
     pub status: ConnectionStatus
 }
 
 impl HistoryRecord {
     pub fn online() -> Self {
         HistoryRecord {
-            timestamp: Utc::now(),
+            timestamp: offset::Local::now(),
             status: ConnectionStatus::Online
         }
     }
 
     pub fn offline() -> Self {
         HistoryRecord {
-            timestamp: Utc::now(),
+            timestamp: offset::Local::now(),
             status: ConnectionStatus::Offline
         }
     }
