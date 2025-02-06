37c1630e40cc86dc9820586c84b533d2dfd3d83b
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-02-06 17:36:58 +0200

    fix: make the table look prettier

--- history/src/table.rs
+++ history/src/table.rs
@@ -20,24 +20,36 @@ impl HistoryTable {
     ) {
         TableBuilder::new(ui)
             .striped(true)
-            .scroll_bar_visibility(ScrollBarVisibility::AlwaysHidden)
+            .scroll_bar_visibility(ScrollBarVisibility::VisibleWhenNeeded)
             .cell_layout(Layout::left_to_right(Align::Center))
             .column(Column::remainder().resizable(false))
             .column(Column::auto().resizable(false))
+            .column(Column::auto().resizable(false))
+            .column(Column::auto().resizable(false))
+            .column(Column::remainder().resizable(false))
             .body(|mut body| {
                 for record in data {
                     body.row(30.0, |mut row| {
+                        row.col(|_| {});
                         row.col(|ui| {
-                            ui.label(RichText::new(format!(
-                                "{}   {}", 
-                                record.timestamp.date_naive().to_string(),
-                                record.timestamp.time().format("%H:%M")
-                            )).size(15.0));
+                            ui.label(RichText::new(
+                                record.timestamp.date_naive()
+                                    .to_string(),
+                            ).size(15.0));
                         });
                         row.col(|ui| {
-                            ui.add_space(20.0);
+                            ui.add_space(5.0);
+                            ui.label(RichText::new(
+                                record.timestamp.time()
+                                    .format("%H:%M")
+                                    .to_string()
+                            ).size(15.0));
+                        });
+                        row.col(|ui| {
+                            ui.add_space(50.0);
                             record.status.as_icon(ui);
                         });
+                        row.col(|_| {});
                     });
                 }
             });
