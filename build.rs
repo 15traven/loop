6d68d4787c0b16b26720adbca8fdda2080f5172d
Mykyta <sikachyna.n@gmail.com>
2025-03-17 16:47:51 +0200

    feat: set manifest

--- build.rs
+++ build.rs
@@ -9,6 +9,17 @@ fn main( ) {
             winapi::um::winnt::LANG_ENGLISH,
             winapi::um::winnt::SUBLANG_ENGLISH_US
         ));
+        res.set_manifest(r#"
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+            <security>
+                <requestedPrivileges>
+                    <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+                </requestedPrivileges>
+            </security>
+        </trustInfo>
+        </assembly>
+        "#);
         res.compile().unwrap();
     }
 }
